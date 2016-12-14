261716e5b6b9bdbfa803f5e79210af6482c38d80
Aykut Yaman <yaman.ayk@gmail.com>
2016-12-14 14:05:35 +0300

    Write applicatives for concurrent actions

--- applicative_for_concurrent.js
+++ applicative_for_concurrent.js
@@ -13,8 +13,10 @@ const Db = ({
 })
 
 const reportHeader = (p1, p2) =>
-  console.log`Report: ${p1.title} compared to ${p2.title}`)
+  `Report: ${p1.title} compared to ${p2.title}`;
 
-Db.find(20).chain(p1 =>
-  Db.find(8).map(p2 =>
-    reportHeader(p1, p2)))
+
+Task.of(p1 => p2 => reportHeader(p1, p2))
+    .ap(Db.find(20))
+    .ap(Db.find(8))
+    .fork(console.error, console.log)
